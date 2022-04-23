d55070e9b2406dcfee8005c5c1097743c6df754d
alkhwarizmi2003 <alkhwarizmi2003@gmail.com>
2022-04-23 07:12:06 +0500

    33

--- lib/DataTableWidget.dart
+++ lib/DataTableWidget.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-
+import 'package:collection/collection.dart';
 import 'SampleData.dart';
 
+class ColumnInfo{
+  final String lable;
+  final String name;
+  final int width;
+  final Type type ;
+
+  ColumnInfo(this.lable, this.name,this.width, this.type);
+}
+
 class DataTableWidget extends StatefulWidget {
+  final List<ColumnInfo> columnInfos;
   final List<String> columns;
   final List<Map<String, dynamic>> data;
 
   DataTableWidget(
+      this.columnInfos,
     this.columns,
     this.data, {
     Key? key,
@@ -20,107 +31,108 @@ class DataTableWidget extends StatefulWidget {
 
 class _DataTableWidgetState extends State<DataTableWidget> {
   ScrollController sc = ScrollController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    sc.addListener(() {
-      print('scrolling ....');
-    });
-  }
+  bool sortAscending = false;
+  int sortColumnIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Container(
-        height: 200,
+        height: 450,
         decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 3)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
               flex: 0,
-              child: DataTable(columnSpacing: 0,
+              child: DataTable(
+                columnSpacing: 0,
                 headingRowColor: MaterialStateColor.resolveWith(
                   (states) {
                     return Colors.lightGreenAccent;
                   },
                 ),
                 // decoration: BoxDecoration(color: Colors.purpleAccent),
-                columns: widget.columns.map((e) => DataColumn(label: Text(e))).toList(),
+                columns: widget.columns
+                    .map(
+                      (e) => DataColumn(
+                        label: Text(e),
+                      ),
+                    )
+                    .toList(),
                 rows: [
                   DataRow(cells: [
                     DataCell(
                       Container(
-                        width: 20,
-child: Text('ert'),
-                      ),
-                    ),
-                    DataCell(
-                      Container(
-                        width: 20,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
                     DataCell(
                       Container(
-                        width: 100,
+                        width: 80,
+                        child: Text('ert'),
+                      ),
+                    ),
+                    DataCell(
+                      Container(
+                        width: 80,
                         child: Text('ert'),
                       ),
                     ),
@@ -132,15 +144,31 @@ child: Text('ert'),
               flex: 1,
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
-                child: DataTable(columnSpacing: 0,
+                child: DataTable(
+                  sortAscending: sortAscending,
+                  sortColumnIndex: sortColumnIndex,
+                  columnSpacing: 0,
                   headingRowHeight: 40,
-                  columns: widget.columns.map((e) => DataColumn(label: Text(e))).toList(),
+                  columns: widget.columns
+                      .map(
+                        (e) => DataColumn(
+                          onSort: (columnIndex, ascending) {
+                            setState(() {
+                              sortColumnIndex = columnIndex;
+                              sortAscending = ascending;
+                            });
+                            print('>>>> $columnIndex, $ascending');
+                          },
+                          label: Text(e),
+                        ),
+                      )
+                      .toList(),
                   rows: widget.data
                       .map(
                         (m) => DataRow(
                             cells: m.entries
                                 .toList()
-                                .map((map) => DataCell(Container(width: 100, child: Text(map.value.toString()))))
+                                .map((map) => DataCell(Container(width: 80, child: Text(map.value.toString()))))
                                 .toList()),
                       )
                       .toList(),
--- lib/DataViewer.dart
+++ lib/DataViewer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_data_table/DataTableWidget.dart';
+import 'package:collection/collection.dart';
 
 import 'SampleData.dart';
 
@@ -12,6 +13,7 @@ class DataViewerPage extends StatefulWidget {
 
 class _DataViewerPageState extends State<DataViewerPage> {
   SampleData sd = SampleData();
+  late List<ColumnInfo> colInfos;
 
   @override
   void initState() {
@@ -22,7 +24,24 @@ class _DataViewerPageState extends State<DataViewerPage> {
     print('>>>> 333  ${sd.transactions.length}');
     sd.getTransData();
 
+     colInfos = [
+      ColumnInfo('ID', 'trans_id', 20, int),
+       ColumnInfo('Hall Id', 'hall_id', 20, int),
+       ColumnInfo('Owner Id', 'owner_id', 20, int),
+       ColumnInfo('User Id', 'user_id', 20, int),
+       ColumnInfo('Rcpt #', 'receipt_no', 20, int),
+       ColumnInfo('Book Dt', 'book_date', 20, int),
+       ColumnInfo('Func Dt', 'function_date', 20, int),
+       ColumnInfo('Func Name', 'function_name', 20, int),
+       ColumnInfo('Shift', 'trans_id', 20, int),
+       ColumnInfo('Party Name', 'trans_id', 20, int),
+       ColumnInfo('Address', 'trans_id', 20, int),
+       ColumnInfo('Contact', 'trans_id', 20, int),
+
+    ];
+
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +49,7 @@ class _DataViewerPageState extends State<DataViewerPage> {
       appBar: AppBar(
         title: Text('Data Table'),
       ),
-      body: DataTableWidget([
+      body: DataTableWidget(colInfos, [
         'Id',
         'Hall Id',
         'Owner Id',
@@ -44,6 +63,42 @@ class _DataViewerPageState extends State<DataViewerPage> {
         'Address',
         'Contact',
       ], sd.getTransData()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          List<Iterable<TmpObj>> list = sd
+              .getTransData()
+              .map((e) => e.entries
+                  .toList()
+                  .mapIndexed((index, element) => TmpObj(index.toString(), element.key, element.value.toString())))
+              .toList();
+
+          Iterable<TmpObj> list2 = sd
+              .getTransData()[0]
+              .entries
+              .toList()
+              .mapIndexed((index, element) => TmpObj(index.toString(), element.key, element.value.toString()));
+
+          list2.forEach((item) {
+            print('>>>>>>>>>>> ${item}');
+          });
+        },
+        tooltip: 'Tool tip',
+        child: Icon(Icons.add),
+      ),
     );
+  }
+}
+
+class TmpObj {
+  final String str1;
+  final String str2;
+  final String str3;
+
+  TmpObj(this.str1, this.str2, this.str3);
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '--- $str2 ';
   }
 }
